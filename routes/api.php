3f100a1a37f7f768556ff6f1aa4b4c4901a8ef95
Paul_Luque <pluquec@unsa.edu.pe>
2024-08-13 18:05:21 -0500

    routes for api module venta

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,46 @@ Route::get('/trabajadores/{id}', [TrabajadorController::class, 'show']);
 Route::post('/trabajadores', [TrabajadorController::class, 'store']);
 Route::put('/trabajadores/{id}', [TrabajadorController::class, 'update']);
 Route::delete('/trabajadores/{id}', [TrabajadorController::class, 'destroy']);
+
+
+// Routes Ventasa
+
+// Listar todas las ventas
+Route::get('/ventas', function () {
+    return response()->json(['message' => 'Listar todas las ventas']);
+});
+
+// Obtener detalles de una venta
+Route::get('/ventas/{id}', function ($id) {
+    return response()->json(['message' => 'Obtener detalles de la venta', 'id' => $id]);
+});
+
+// Listar detalles de una venta
+Route::get('/ventas/{id}/detalles', function ($id) {
+    return response()->json(['message' => 'Listar detalles de la venta', 'id' => $id]);
+});
+
+// Crear una nueva venta
+Route::post('/ventas', function () {
+    return response()->json(['message' => 'Crear una nueva venta']);
+});
+
+// Actualizar una venta
+Route::put('/ventas/{id}', function ($id) {
+    return response()->json(['message' => 'Actualizar una venta', 'id' => $id]);
+});
+
+// Eliminar (inactivar) una venta
+Route::delete('/ventas/{id}', function ($id) {
+    return response()->json(['message' => 'Eliminar (inactivar) una venta', 'id' => $id]);
+});
+
+// Crear un nuevo detalle de venta
+Route::post('/ventas/{id}/detalles', function ($id) {
+    return response()->json(['message' => 'Crear un nuevo detalle de venta', 'id' => $id]);
+});
+
+// Listar todos los detalles de venta
+Route::get('/ventas/detalles', function () {
+    return response()->json(['message' => 'Listar todos los detalles de venta']);
+});
